d1fd6c7448f0d44cc7251515fbfa0c4029ba0d92
maatii022 <mati.vio02@gmail.com>
2026-03-25 16:10:29 +0000

    Mejoras dashboard 2.1.1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1377,7 +1377,7 @@ function PackCard({
                     disabled={loading}
                     variant="secondary"
                   >
-                    Reemplazo
+                    Reemplazo.
                   </ActionButton>
                 )}
               </div>
